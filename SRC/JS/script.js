cb291029151fa0249e58028f25e7f0afe0614326
Nandun Senanayaka <nandunsenanayaka21@gmail.com>
2024-11-28 12:49:20 +0530

    add navbar menu js part

--- SRC/JS/script.js
+++ SRC/JS/script.js
@@ -19,6 +19,12 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+document.getElementById('menu-toggle').addEventListener('click', () => {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('hidden');
+});
+
+
 
 
 
